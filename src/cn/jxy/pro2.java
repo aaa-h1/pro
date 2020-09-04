786e0a72becbc8ac5cc913fe2025df73297189f5
nmnmnnm <3118787713@qq.com>
2020-09-04 19:09:05 +0800

    sdafsa

---
+++ src/cn/jxy/pro2.java
@@ -0,0 +1,13 @@
+package cn.jxy;
+
+public class pro2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fdsafsda");
+	}
+
+}
